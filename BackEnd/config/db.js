17a7babdcb1582906adf64d98b1acc204701e164
Aryan Kumar Barnwal <164518235+Aryankumarbarnwal@users.noreply.github.com>
2026-06-19 16:58:01 +0530

    Update db.js

--- BackEnd/config/db.js
+++ BackEnd/config/db.js
@@ -1,12 +1,11 @@
-import mongoose from "mongoose";
-const connectDb = async () =>{
-    try{
-        await mongoose.connect(process.env.MongoDB_URL);
+import mongoose from "mongoose"; 
+const connectDb = async () =>{ 
+    try{ await mongoose.connect(process.env.MongoDB_URL); 
         console.log("DB connected")
-    }catch(error){
-        console.log("DB Error")
-    }
-
-}
-
+    }catch(error)
+    { 
+        console.log("DB Error");
+    } 
+} 
+    
 export default connectDb
